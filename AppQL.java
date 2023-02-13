a0bb74a7858a71313ac5bb5cd50e19d86223a87d
Tran Viet Vuong <105685671+littlebadboy9x@users.noreply.github.com>
2023-02-13 08:42:46 +0700

    Add files via upload

---
+++ AppQL.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package application;
+
+/**
+ *
+ * @author Tran Viet Vuong
+ */
+public class AppQL {
+    public static void main(String[] args) {
+        
+    }
+}
